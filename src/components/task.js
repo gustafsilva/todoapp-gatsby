efe78a13e2844c140ab3f02278f0cbbfe2a7ac22
Gustavo Silva <gustafsilva@hotmail.com>
2018-11-06 17:01:21 -0200

    New function add task

--- src/components/task.js
+++ src/components/task.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
 class Task extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderButton() {
+    let button;
+    console.log(this.props.status);
+    if(this.props.status) {
+      button = <a className="button">{this.props.description}</a>
+    }
+    else {
+      button = <a className="button has-text-dangered">{this.props.description}</a>
+    } 
+    
+    return( button );
+  }
+
   render() {
     return(
       <p className="task control">
-        <a className="button">
-          Task
-        </a>
+        { this.renderButton() }
       </p>
     );
   }
--- src/components/todoapp.js
+++ src/components/todoapp.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+
 import Task from "./task";
+
 
 class TodoApp extends Component {
   constructor(props) {
@@ -10,48 +12,68 @@ class TodoApp extends Component {
       tasks: []
     };
 
+    this.setNewTask = this.setNewTask.bind(this);
     this.addTask = this.addTask.bind(this);
   }
 
+  setNewTask(event) {
+    if (event.key == "Enter") {
+      this.addTask();
+    }
+    else {
+      this.setState({
+        ...this.state,
+        newTask: event.target.value
+      });
+    }
+  }
+
   addTask() {
+    const { newTask } = this.state;
     let { tasks } = this.state;
 
-    const task = {
-      description: this.state.newTask,
-      status: false
-    }
-    tasks.push(task);
-
-    this.setState((state) => {
-      return {
+    if(newTask !== "") {
+      const task = {
+        description: this.state.newTask,
+        status: false
+      }
+      tasks.push(task);
+  
+      this.setState({
         newTask: "",
         tasks
-      }
+      });
+    }
+  }
+
+  renderTasks() {
+    const { tasks } = this.state;
+
+    const tasksRendered = tasks.map((task) => {
+      return (
+        <Task description={task.description} status={tasks.status} />
+      )
     });
 
-    console.log(this.state);
+    return (
+      tasksRendered
+    );
   }
 
   render() {
     return (
       <div className="todoapp content">
-        <div className="new-task field has-addons">
+        <div className="new-task form field has-addons">
           <div className="control">
-            <input className="input" type="text" placeholder="Add new task" ></input>
+            <input className="input" type="text" placeholder="Add new task" value={this.state.newTask} onChange={this.setNewTask} onKeyPress={this.setNewTask}></input>
           </div>
           <div className="control">
-            <button className="button is-success" type="submit" onClick={this.addTask}>Add</button>
+            <button className="button is-success" onClick={this.addTask}>Add</button>
           </div>
         </div>
 
         <div className="tasks field is-grouped is-grouped-multiline">
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
+          {this.renderTasks()}
         </div>
       </div>
     );
