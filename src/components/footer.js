cb97a722cfb5213c5c55c357d287d7fcb83071e9
Gustavo Silva <gustafsilva@hotmail.com>
2018-11-07 20:17:41 -0200

    Adjusting footer and task completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ export default class Footer extends Component {
 
             </a><strong>TodoApp</strong> by <a href="www.github.com/gustafsilva">Gustavo F. Silva</a>.
 
-            The source code is licensed<a href="https://github.com/gustafsilva/todoapp-gatsby/blob/feature-remove-warnings/LICENSE">MIT</a>
+            The source code is licensed<a href="https://github.com/gustafsilva/todoapp-gatsby/blob/feature-remove-warnings/LICENSE"> MIT</a>
             <i className="icon has-text-danger"><FaHeart /></i>
           </p>
         </div>
