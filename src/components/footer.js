805944bb9d8ce3e6df06f354852ba606d67020a1
Gustavo Silva <gustafsilva@hotmail.com>
2018-11-08 23:40:34 -0200

    Documenting and refactoring component header and footer :scroll:

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { FaHeart, FaGithub } from 'react-icons/fa'
 
-export default class Footer extends Component {
-  render () {
-    return (
-      <footer className="footer">
-        <div className="content">
-          <p>
-            <a className={ 'icon' } href="https://www.github.com/gustafsilva/todoapp-gatsby"><FaGithub />
+/**
+ * Component responsible for the Footer, contains general information
+ * about the site and code such as license add external links to the
+ * project and author.
+ */
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content">
+        <p>
+          <a className={'icon'} href="https://www.github.com/gustafsilva/todoapp-gatsby"><FaGithub />
 
-            </a><strong>TodoApp</strong> by <a href="https://www.github.com/gustafsilva">Gustavo F. Silva</a>.
+          </a><strong>TodoApp</strong> by <a href="https://www.github.com/gustafsilva">Gustavo F. Silva</a>.
 
             The source code is licensed<a href="https://github.com/gustafsilva/todoapp-gatsby/blob/master/LICENSE"> MIT</a>
-            <i className="icon has-text-danger"><FaHeart /></i>
-          </p>
-        </div>
-      </footer>
-    )
-  }
+          <i className="icon has-text-danger"><FaHeart /></i>
+        </p>
+      </div>
+    </footer>
+  )
 }
+
+export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { siteMetadata } from '../../gatsby-config'
 
-export default class Header extends Component {
-  render () {
-    const { title, description } = siteMetadata
+/**
+ * Component responsible for the Header of the app, will contain
+ * information about the author, image and external links.
+ */
+const Header = () => {
+  /* Capturing information about the title and description of the site. */
+  const { title, description } = siteMetadata
 
-    return (
-      <section className="hero is-info">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">{title}</h1>
-            <h2 className="subtitle">{description}</h2>
-          </div>
+  return (
+    <section className="hero is-info">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">{title}</h1>
+          <h2 className="subtitle">{description}</h2>
         </div>
-      </section>
-    )
-  }
+      </div>
+    </section>
+  )
 }
+
+export default Header
